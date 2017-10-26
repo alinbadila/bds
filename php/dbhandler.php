f849c75538f4e255eb0c24b4d807b62ec0122f84
Alin Badila <badila.alin@yahoo.com>
2017-10-26 22:32:23 +0300

    Setat nume BDS cu majuscule pentru compatibilitate server linux

--- php/dbhandler.php
+++ php/dbhandler.php
@@ -1,5 +1,5 @@
 <?php
-$conn = mysqli_connect("localhost", "root", "Alin1984", "bds");
+$conn = mysqli_connect("localhost", "root", "Alin1984", "BDS");
 if (! $conn ) {
 	# conectare nereusita
 	die("Conectare nereușită:".mysqli_connect_error());
