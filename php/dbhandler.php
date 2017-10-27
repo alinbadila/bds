29f2046e2d2a611e3b5379745dd5a09b2aacd9a4
Alin Badila <badila.alin@yahoo.com>
2017-10-27 16:57:02 +0300

    Specificat server principal si unic mysql

--- php/dbhandler.php
+++ php/dbhandler.php
@@ -1,5 +1,11 @@
 <?php
-$conn = mysqli_connect("localhost", "root", "Alin1984", "BDS");
+/*$conn = mysqli_connect("localhost", "root", "Alin1984", "BDS");*/
+$servername = "86.125.149.136";
+$username = "root";
+$password = "Alin1984";
+$database = "BDS";
+$port = 3306;
+$conn = mysqli_connect($servername, $username, $password, $database, $port);
 if (! $conn ) {
 	# conectare nereusita
 	die("Conectare nereușită:".mysqli_connect_error());
