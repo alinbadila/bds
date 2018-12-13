faf25057c8385e5d863a4528d637a68e70132041
Alin Badila <badila.alin@yahoo.com>
2018-12-13 12:02:29 +0200

    - bara de navigare standardizata

--- php/prezenta.php
+++ php/prezenta.php
@@ -5,39 +5,38 @@
 
 <html lang="ro">
 <head>
-    <title>B.D.S. - Prezența</title>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name='description' content='Managementul unei subunitati de pompieri'>
-    <meta name='keywords' content='pompieri, interventie, calendar, organizare'>
-    <meta http-equiv='content-type' content='text/html; charset=utf-8'>
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    
-    <meta name="author" content="Alin Bădilă, badila.alin@yahoo.com">
-    
-    <link rel="shortcut icon" href="../favicon.png" />
-    <link rel="apple-touch-icon" href="../faviconsafari.png" />
-	<link rel='stylesheet' type='text/css' href='../css/style.css'>
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <meta name='description' content='Managementul unei subunitati de pompieri'>
+  <meta name='keywords' content='pompieri, interventie, calendar, organizare'>
+  <meta http-equiv='content-type' content='text/html; charset=utf-8'>
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="author" content="Alin Bădilă, badila.alin@yahoo.com">
+
+  <title>B.D.S. - Prezența</title>
+
 	<link rel="stylesheet" type="text/css" href="../css/bootstrap.css">
+  <link rel="stylesheet" type="text/css" href="../css/style.css">
 </head>
 
 <body>
-	<nav class="navbar navbar-dark navbar-xs" style="background-color: #222222;"> 
-		<!-- Navbar content -->
-		 <div class="navbar-header">
-		 	<a class="navbar-brand" style="font-size:80%;" href="../index.php">Baza de date a subunității</a>
-		</div>
-		<ul class="nav navbar-nav">
-			<li><a href="../index.php">Acasă</a></li>			
-		</ul>
-		<ul class="nav navbar-nav navbar-right">
-			<li><span class="navbar-text" style="font-size:60%;"><?php echo $_SESSION['username'];?>
-			<a href="php/deconectare.php"><img src="../icons/logout.png"></a>
-			</span></li>
-		</ul>
-	</nav>
-	
+  <!-- HEADERUL si BARA DE NAVIGARE -->
+  <header class="top" role="header">
+    <div class="container">
+      <!-- Continut bara de navigare -->
+      <nav class="navbar navbar-dark navbar-xs" role="navigation" style="background-color: #222222;">
+    	  <a class="navbar-brand navbar-header" style="font-size:80%;" href="#">Baza de date a subunității</a>
+    		<ul class="nav navbar-nav navbar-right">
+    			<li>
+            <span class="navbar-text" style="font-size:60%;"><?php echo $_SESSION['username'];?>
+    			    <a href="php/deconectare.php"><img src="icons/logout.png"></a>
+    			  </span>
+          </li>
+    		</ul>
+    	</nav>
+    </div>
+  </header>
+
 	<div class="container-fluid">
         <h2>Prezența cadrelor</h2>
         <div class="iconGrid">
@@ -60,10 +59,11 @@
         </div>
         <footer><a href='../index.php'>Acasă</a> | <a>Contact</a></footer>
     </div>
-    
-    <script type='text/javascript' src='../js/jquery.js'></script>
-	<script type='text/javascript' src='../js/bootstrap.js'></script>
-    <?php else: 
+
+    <script type='text/javascript' src='..js/jquery.min.js'></script>
+    <script type='text/javascript' src='..js/popper.min.js'></script>
+  	<script type='text/javascript' src='..js/bootstrap.min.js'></script>
+<?php else:
       header("Location: loginpage.php");
       endif; ?>
 </body>
