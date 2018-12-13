3a73ec0f8b831692ec6676e86e0f43a9379534ec
Alin Badila <badila.alin@yahoo.com>
2018-12-13 12:28:58 +0200

    sters tag 'site_content'

--- php/angajati.php
+++ php/angajati.php
@@ -37,30 +37,29 @@
     	</nav>
     </div>
   </header>
-    <div id='site_content'>
-        <h2>Personalul subunității</h2>
 
-        <div class="iconGrid">
-            <div class="iconContainer">
-                <a href="angajati8ore.php"><img class="iconClass" src="../icons/8ore.png" alt="Angajati 8 ore">
-                    <span class="caption">Program de 8 ore</span></a>
-            </div>
+  <h2>Personalul subunității</h2>
 
-            <div class="iconContainer">
-                <a href="angajatitura1.php"><img class="iconClass" src="../icons/1.png" alt="Tura 1"><span class="caption">Tura 1</span></a>
-            </div>
+  <div class="iconGrid">
+      <div class="iconContainer">
+          <a href="angajati8ore.php"><img class="iconClass" src="../icons/8ore.png" alt="Angajati 8 ore">
+              <span class="caption">Program de 8 ore</span></a>
+      </div>
 
-            <div class="iconContainer">
-                <a href="angajatitura2.php"><img class="iconClass" src="../icons/2.png" alt="Tura 2"><span class="caption">Tura 2</span></a>
-            </div>
+      <div class="iconContainer">
+          <a href="angajatitura1.php"><img class="iconClass" src="../icons/1.png" alt="Tura 1"><span class="caption">Tura 1</span></a>
+      </div>
 
-            <div class="iconContainer">
-                <a href="angajatitura3.php"><img class="iconClass" src="../icons/3.png" alt="Tura 3"><span class="caption">Tura 3</span></a>
-            </div>
-        </div>
+      <div class="iconContainer">
+          <a href="angajatitura2.php"><img class="iconClass" src="../icons/2.png" alt="Tura 2"><span class="caption">Tura 2</span></a>
+      </div>
 
-        <footer><a href='../index.php'>Acasă</a> | <a>Contact</a></footer>
-    </div>
+      <div class="iconContainer">
+          <a href="angajatitura3.php"><img class="iconClass" src="../icons/3.png" alt="Tura 3"><span class="caption">Tura 3</span></a>
+      </div>
+  </div>
+
+    <footer><a href='../index.php'>Acasă</a> | <a>Contact</a></footer>
 
     <script type='text/javascript' src='../js/jquery.min.js'></script>
     <script type='text/javascript' src='../js/popper.min.js'></script>
