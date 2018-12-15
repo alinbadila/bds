30d142f30818cd9c3ef3ba469723867f5fce7f39
Alin Badila <badila.alin@yahoo.com>
2018-12-15 03:56:33 +0200

    sters links din card-footer

--- php/loginpage.php
+++ php/loginpage.php
@@ -52,7 +52,7 @@
           if (strpos($url, 'error=nouser') !== false) {
               # code...
               echo '<div class="card-footer">'.
-              				'<div class="d-flex justify-content-center links">'.
+              				'<div class="d-flex justify-content-center">'.
               					'<h6>Utilizatorul nu există în baza de date!</h6>'.
               				'</div>'.
               			'</div>';
