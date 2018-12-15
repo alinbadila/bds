1b560365d99a697029b1fc4ab96b21192c5ffe7d
Alin Badila <badila.alin@yahoo.com>
2018-12-15 04:29:22 +0200

    corecturi footer

--- index.php
+++ index.php
@@ -41,86 +41,45 @@
     </div>
   </header>
 
-  <!-- <div class="container-fluid">
-
-      <div class="iconGrid">
-          <div class="iconContainer">
+  <div class="container-fluid">
+      <div class="row">
+          <div class="col-md-3 iconContainer">
               <a><img class="iconClass" src="icons/organizare.png" alt="Registru organizare"><span class="caption">Registru organizare</span></a>
           </div>
 
-          <div class="iconContainer">
+          <div class="col-md-3 iconContainer">
               <a href="php/prezenta.php"><img class="iconClass" src="icons/prezenta.png" alt="Prezenta"><span class="caption">Prezența</span></a>
           </div>
 
-          <div class="iconContainer">
+          <div class="col-md-3 iconContainer">
               <a href="php/calendar.php"><img class="iconClass" src="icons/calendar.png" alt="Agenda zilei"><span class="caption">Agenda zilei</span></a>
           </div>
 
-          <div class="iconContainer">
+          <div class="col-md-3 iconContainer">
               <a href="php/angajati.php"><img class="iconClass" src="icons/angajat.png" alt="Angajati"><span class="caption">Angajați</span></a>
           </div>
       </div>
 
-      <div class="iconGrid">
-          <div class="iconContainer">
+      <div class="row">
+          <div class="col-md-3 iconContainer">
               <a><img class="iconClass" src="icons/autospeciala_incendiu.png" alt="Raport interventie"><span class="caption">Raport intervenție</span></a>
           </div>
 
-          <div class="iconContainer">
+          <div class="col-md-3 iconContainer">
               <a><img class="iconClass" src="icons/ambulanta.png" alt="Raport SMURD"><span class="caption">Raport SMURD</span></a>
           </div>
 
-          <div class="iconContainer">
+          <div class="col-md-3 iconContainer">
               <a><img class="iconClass" src="icons/raport_desca.png" alt="Raport descarcerare"><span class="caption">Raport descarcerare</span></a>
           </div>
 
-          <div class="iconContainer">
+          <div class="col-md-3 iconContainer">
               <a><img class="iconClass" src="icons/cautare.png" alt="Cauta raport"><span class="caption">Caută raport</span></a>
           </div>
       </div>
 
-      <footer><a>Contact</a></footer>
-    </div> -->
-
-    <div class="container-fluid">
-        <div class="row">
-            <div class="col-md-3 iconContainer">
-                <a><img class="iconClass" src="icons/organizare.png" alt="Registru organizare"><span class="caption">Registru organizare</span></a>
-            </div>
-
-            <div class="col-md-3 iconContainer">
-                <a href="php/prezenta.php"><img class="iconClass" src="icons/prezenta.png" alt="Prezenta"><span class="caption">Prezența</span></a>
-            </div>
-
-            <div class="col-md-3 iconContainer">
-                <a href="php/calendar.php"><img class="iconClass" src="icons/calendar.png" alt="Agenda zilei"><span class="caption">Agenda zilei</span></a>
-            </div>
-
-            <div class="col-md-3 iconContainer">
-                <a href="php/angajati.php"><img class="iconClass" src="icons/angajat.png" alt="Angajati"><span class="caption">Angajați</span></a>
-            </div>
-        </div>
-
-        <div class="row">
-            <div class="col-md-3 iconContainer">
-                <a><img class="iconClass" src="icons/autospeciala_incendiu.png" alt="Raport interventie"><span class="caption">Raport intervenție</span></a>
-            </div>
-
-            <div class="col-md-3 iconContainer">
-                <a><img class="iconClass" src="icons/ambulanta.png" alt="Raport SMURD"><span class="caption">Raport SMURD</span></a>
-            </div>
-
-            <div class="col-md-3 iconContainer">
-                <a><img class="iconClass" src="icons/raport_desca.png" alt="Raport descarcerare"><span class="caption">Raport descarcerare</span></a>
-            </div>
-
-            <div class="col-md-3 iconContainer">
-                <a><img class="iconClass" src="icons/cautare.png" alt="Cauta raport"><span class="caption">Caută raport</span></a>
-            </div>
-        </div>
-
-        <footer><a>Contact</a></footer>
-      </div>
+      <footer class="navbar-xs"><a>Contact</a></footer>
+    </div>
 
   <script type='text/javascript' src='js/jquery.min.js'></script>
   <script type='text/javascript' src='js/popper.min.js'></script>
