d2813a026f9f811eda6de4d9402589ee73e2f061
Alin Badila <badila.alin@yahoo.com>
2019-01-27 23:50:54 +0200

    test

--- php/calendar.php
+++ php/calendar.php
@@ -50,7 +50,7 @@
       <div class="row">
           <div class="col-lg-12 text-center">
               <h2>Agenda subunității</h2>
-              <?php echo "<h2>" . date("d/m/Y") . "</h2>"?>
+              <?php echo "<p class=\"text-success\">" . date("d/m/Y") . "</p>"?>
           </div>
       </div>
       <div class="row">
