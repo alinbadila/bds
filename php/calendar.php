2beca48ee979fe049e6ebaeffc09e8c26ec380b1
Alin Badila <badila.alin@yahoo.com>
2019-01-27 22:25:43 +0200

    sters tot codul despre concedii, lasat procedura

--- php/calendar.php
+++ php/calendar.php
@@ -62,69 +62,11 @@
               <h3>Concedii de odihna</h3>
           </div>
       </div>
-
+      <!-- Tabel concedii de odihna-->
       <?php
         include 'apicalendar.php';
         afiseazaConcedii();
       ?>
-
-      <!-- Tabel concedii de odihna-->
-      <div class="row">
-        <table class="table table-dark table-striped">
-          <thead class="thead-light">
-            <tr>
-              <th>Nr.crt.</th>
-              <th>Grad</th>
-              <th>Nume și prenume</th>
-              <th>Data inceput</th>
-              <th>Data sfarsit</th>
-              <th>Tip concediu</th>
-              <th>Tura</th>
-            </tr>
-          </thead>
-          <tbody>
-            <?php
-                try {
-                    $conn = new PDO('mysql:host=aluna.go.ro;dbname=BDS;charset=utf8', $_SESSION['dbusername'], $_SESSION['dbpassword']);
-                } catch (Exception $e) {
-                    die('Eroare : '.$e->getMessage());
-                }
-                $sql = "SELECT  BDS.grade.GRAD, BDS.date_pers.NUME, BDS.date_pers.PRENUME,
-		                            BDS.concedii.DATA_INCEPUT, BDS.concedii.DATA_SFARSIT, BDS.concedii.TIP,
-                                BDS.functii.TURA
-	                      FROM BDS.grade
-			                          INNER JOIN BDS.concedii ON BDS.grade.ID_CADRU = BDS.concedii.ID_CADRU
-			                          INNER JOIN BDS.date_pers ON BDS.grade.ID_CADRU = BDS.date_pers.ID_CADRU
-                                INNER JOIN BDS.functii ON BDS.grade.ID_CADRU = BDS.functii.ID_CADRU
-                        WHERE (BDS.concedii.VALID = 1) and (BDS.functii.ID_CADRU IS NOT NULL) and
-                              (CURDATE() >= BDS.concedii.DATA_INCEPUT and CURDATE() <= BDS.concedii.DATA_SFARSIT)
-                        ORDER BY BDS.functii.TURA;";
-                $query = $conn->prepare($sql);
-                $query -> execute();
-                $rezultat = $query -> fetchAll();
-                if (!$rezultat) {
-                    echo "Nicio persoana nu se afla in concediu de odihna sau suplimentar in data mentionata.";
-                } else {
-                    $nrcrt = 0;
-                    foreach ($rezultat as $rand) {
-                        $nrcrt++;
-                        echo "<tr>";
-                        echo "<td>" . $nrcrt . "</td>";
-                        echo "<td>" . $rand["GRAD"] . "</td>";
-                        echo "<td>" . $rand["NUME"] . " " . $rand["PRENUME"] . "</td>";
-                        echo "<td>" . $rand["DATA_INCEPUT"] . "</td>";
-                        echo "<td>" . $rand["DATA_SFARSIT"] . "</td>";
-                        echo "<td>" . $rand["TIP"] . "</td>";
-                        echo "<td>" . $rand["TURA"] . "</td>";
-                        echo "</tr>";
-                    }
-                }
-                $query -> closeCursor();
-                $conn = null;
-                ?>
-          </tbody>
-        </table>
-      </div>
 
       <div class="row">
           <div class="col-lg-12 text-left">
