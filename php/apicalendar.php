d7de6d03f0f0fe0fd724930acac85d988fa111c9
Alin Badila <badila.alin@yahoo.com>
2019-01-27 22:46:50 +0200

    tyrecut procedura co in php baza

--- php/apicalendar.php
+++ php/apicalendar.php
@@ -54,68 +54,67 @@
     $query -> closeCursor();
     $conn = null;
   }
-?>
 
-
-<!-- Procedura afiseaza in tabel persoanele aflate in concediu medical la data primita ca parametru-->
-<?php
-  function afiseazaConcediiMedicale() {
-    try {
-        $conn = new PDO('mysql:host=aluna.go.ro;dbname=BDS;charset=utf8', $_SESSION['dbusername'], $_SESSION['dbpassword']);
-    } catch (Exception $e) {
-        die('Eroare : '.$e->getMessage());
-    }
-    echo "sunt in procedura concedii medcale";
-    $sql = "SELECT  BDS.grade.GRAD, BDS.date_pers.NUME, BDS.date_pers.PRENUME,
-                    BDS.concedii_medicale.DATA_INCEPUT, BDS.concedii_medicale.DATA_SFARSIT,
-                    BDS.concedii_medicale.DIAGNOSTIC, BDS.concedii_medicale.COD_DIAGNOSTIC, BDS.functii.TURA
-            FROM BDS.grade
-                    INNER JOIN BDS.concedii_medicale ON BDS.grade.ID_CADRU = BDS.concedii_medicale.ID_CADRU
-                    INNER JOIN BDS.date_pers ON BDS.grade.ID_CADRU = BDS.date_pers.ID_CADRU
-                    INNER JOIN BDS.functii ON BDS.grade.ID_CADRU = BDS.functii.ID_CADRU
-            WHERE (BDS.functii.ID_CADRU IS NOT NULL) and
-                  (CURDATE() >= BDS.concedii_medicale.DATA_INCEPUT and CURDATE() <= BDS.concedii_medicale.DATA_SFARSIT)
-            ORDER BY BDS.functii.TURA;";
-    $query = $conn->prepare($sql);
-    $query -> execute();
-    $rezultat = $query -> fetchAll();
-    if (!$rezultat) {
-        echo "Nicio persoana nu se afla in concediu medical la data mentionata.";
-    } else {
-        echo  "<table class=\"table table-dark table-striped\">";
-          echo "<thead class=\"thead-light\">";
-            echo "<tr>";
-              echo "<th>Nr.crt.</th>";
-              echo "<th>Grad</th>";
-              echo "<th>Nume și prenume</th>";
-              echo "<th>Data inceput</th>";
-              echo "<th>Data sfarsit</th>";
-              echo "<th>Diagnostic</th>";
-              echo "<th>Cod diagnostic</th>";
-              echo "<th>Tura</th>";
-            echo "</tr>";
-          echo "</thead>";
-          echo "<tbody>";
-              $nrcrt = 0;
-              foreach ($rezultat as $rand) {
-                  $nrcrt++;
-                  echo "<tr>";
-                  echo "<td>" . $nrcrt . "</td>";
-                  echo "<td>" . $rand["GRAD"] . "</td>";
-                  echo "<td>" . $rand["NUME"] . " " . $rand["PRENUME"] . "</td>";
-                  echo "<td>" . $rand["DATA_INCEPUT"] . "</td>";
-                  echo "<td>" . $rand["DATA_SFARSIT"] . "</td>";
-                  echo "<td>" . $rand["DIAGNOSTIC"] . "</td>";
-                  echo "<td>" . $rand["COD_DIAGNOSTIC"] . "</td>";
-                  echo "<td>" . $rand["TURA"] . "</td>";
-                  echo "</tr>";
-                }
-          echo "</tbody>";
-        echo "</table>";
-        }
-    $query -> closeCursor();
-    $conn = null;
+/* Procedura afiseaza in tabel persoanele aflate in concediu medical la data primita ca parametru */
+function afiseazaConcediiMedicale() {
+  try {
+      $conn = new PDO('mysql:host=aluna.go.ro;dbname=BDS;charset=utf8', $_SESSION['dbusername'], $_SESSION['dbpassword']);
+  } catch (Exception $e) {
+      die('Eroare : '.$e->getMessage());
   }
+  echo "sunt in procedura concedii medcale";
+  $sql = "SELECT  BDS.grade.GRAD, BDS.date_pers.NUME, BDS.date_pers.PRENUME,
+                  BDS.concedii_medicale.DATA_INCEPUT, BDS.concedii_medicale.DATA_SFARSIT,
+                  BDS.concedii_medicale.DIAGNOSTIC, BDS.concedii_medicale.COD_DIAGNOSTIC, BDS.functii.TURA
+          FROM BDS.grade
+                  INNER JOIN BDS.concedii_medicale ON BDS.grade.ID_CADRU = BDS.concedii_medicale.ID_CADRU
+                  INNER JOIN BDS.date_pers ON BDS.grade.ID_CADRU = BDS.date_pers.ID_CADRU
+                  INNER JOIN BDS.functii ON BDS.grade.ID_CADRU = BDS.functii.ID_CADRU
+          WHERE (BDS.functii.ID_CADRU IS NOT NULL) and
+                (CURDATE() >= BDS.concedii_medicale.DATA_INCEPUT and CURDATE() <= BDS.concedii_medicale.DATA_SFARSIT)
+          ORDER BY BDS.functii.TURA;";
+  $query = $conn->prepare($sql);
+  $query -> execute();
+  $rezultat = $query -> fetchAll();
+  if (!$rezultat) {
+      echo "Nicio persoana nu se afla in concediu medical la data mentionata.";
+  } else {
+      echo  "<table class=\"table table-dark table-striped\">";
+        echo "<thead class=\"thead-light\">";
+          echo "<tr>";
+            echo "<th>Nr.crt.</th>";
+            echo "<th>Grad</th>";
+            echo "<th>Nume și prenume</th>";
+            echo "<th>Data inceput</th>";
+            echo "<th>Data sfarsit</th>";
+            echo "<th>Diagnostic</th>";
+            echo "<th>Cod diagnostic</th>";
+            echo "<th>Tura</th>";
+          echo "</tr>";
+        echo "</thead>";
+        echo "<tbody>";
+            $nrcrt = 0;
+            foreach ($rezultat as $rand) {
+                $nrcrt++;
+                echo "<tr>";
+                echo "<td>" . $nrcrt . "</td>";
+                echo "<td>" . $rand["GRAD"] . "</td>";
+                echo "<td>" . $rand["NUME"] . " " . $rand["PRENUME"] . "</td>";
+                echo "<td>" . $rand["DATA_INCEPUT"] . "</td>";
+                echo "<td>" . $rand["DATA_SFARSIT"] . "</td>";
+                echo "<td>" . $rand["DIAGNOSTIC"] . "</td>";
+                echo "<td>" . $rand["COD_DIAGNOSTIC"] . "</td>";
+                echo "<td>" . $rand["TURA"] . "</td>";
+                echo "</tr>";
+              }
+        echo "</tbody>";
+      echo "</table>";
+      }
+  $query -> closeCursor();
+  $conn = null;
+}
+
+
 ?>
 
 <!-- Procedura afiseaza in tabel persoanele aflate la cursuri in data primita ca parametru-->
