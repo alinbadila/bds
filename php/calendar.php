300a4150088c3cf53bbda257e1745a08d8391529
Alin Badila <badila.alin@yahoo.com>
2019-01-30 16:32:50 +0200

    test

--- php/calendar.php
+++ php/calendar.php
@@ -57,7 +57,7 @@
         <div class="col-lg-12 text-center">
           <p> Alege altă dată: <input type="text" id="datepicker" maxlength="20" size="25" readonly>
               <input class="btn btn-primary btn-sm" id="butonOkData" type="submit" value="ok">
-              Data selectata: <input type="text" id="rezdatepicker" maxlength="10" size="12">
+              Data selectata: <input type="text" id="rezdatepicker" maxlength="10" size="12" readonly>
           <p>
         </div>
       </div>
@@ -145,7 +145,7 @@
               var dateObject = $(this).datepicker('getDate');
             },
             dateFormat:'yy-mm-dd'});
-
+    document.getElementById('rezdatepicker').value = document.getElementById('datepicker').value;
   </script>
 
 <?php else:
