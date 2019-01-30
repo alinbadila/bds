4b8916f88ad29f4bd47f5f82594e6cf81b30e469
Alin Badila <badila.alin@yahoo.com>
2019-01-30 17:52:38 +0200

    test

--- php/calendar.php
+++ php/calendar.php
@@ -25,9 +25,9 @@
 
     <title>B.D.S. - Calendar</title>
 
-    <link rel='stylesheet' type='text/css' href='../css/jquery-ui.css'>
+    <link rel="stylesheet" type="text/css" href="../css/jquery-ui.css">
 	  <link rel="stylesheet" type="text/css" href="../css/bootstrap.min.css">
-    <link rel='stylesheet' type='text/css' href='../css/style.css'>
+    <link rel="stylesheet" type="text/css" href="../css/style.css">
 </head>
 
 <body>
