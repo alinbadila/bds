e79965918d98dead25152d615289d5e1877c1981
Alin Badila <badila.alin@yahoo.com>
2019-01-30 16:06:01 +0200

    test

--- php/calendar.php
+++ php/calendar.php
@@ -55,7 +55,7 @@
       </div>
       <div class="row">
         <div class="col-lg-12 text-center">
-          <p> Alege altă dată: <input type="text" id="datepicker" maxlength="10" size="12" readonly>
+          <p> Alege altă dată: <input type="text" id="datepicker" maxlength="20" size="25" readonly>
               <input class="btn btn-primary btn-sm" id="butonOkData" type="submit" value="ok">
               Data selectata: <input type="text" id="rezdatepicker" maxlength="10" size="12" readonly>
           <p>
